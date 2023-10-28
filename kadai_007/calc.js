085a938d0d93379275303170f8e095db26bb5835
Mieux <142778688+Mirtea@users.noreply.github.com>
2023-10-28 10:59:21 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+// 変数numにランダムな整数を代入
+let num = Math.floor(Math.random() * 50);
+
+// 条件分岐if文
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else {
+  console.log(num);
+}
