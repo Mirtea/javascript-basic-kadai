74cf05d4ab132fa181ffd3f3fe2f7464d6d57779
Mieux <142778688+Mirtea@users.noreply.github.com>
2023-10-30 20:41:56 +0900

    square.jsの更新

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,5 +1,7 @@
-const calculateTotal = (price) => {
-  console.log(price * 2);
+// 与えられた引数numを2倍に、その値を戻り値として返す関数を定義する
+const double = (num) => {
+  return num * 2;
 };
 
-calculateTotal(500);
+// 関数の戻り値を出力する
+console.log(double(50));
